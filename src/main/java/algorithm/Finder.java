0c9e6b71a286d1638794229ac8473caf4dd55886
Juan Sánchez <jsmontesinos@gmail.com>
2018-12-16 14:31:00 +0100

    Convert Finder to a wrapper over new PersonFinder class

--- src/main/java/algorithm/Finder.java
+++ src/main/java/algorithm/Finder.java
@@ -1,57 +1,15 @@
 package algorithm;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Finder {
-	private final List<Person> people;
+	private PersonFinder personFinder;
 
 	public Finder(List<Person> people) {
-		this.people = people;
+		this.personFinder = new PersonFinder(people);
 	}
 
 	public PersonPair Find(SearchType searchType) {
-		List<PersonPair> personPairList = getPersonPairs();
 
-		return personPairList.isEmpty() ? new PersonPair() : calculatePairAccordingType(searchType, personPairList);
-	}
-
-	private PersonPair calculatePairAccordingType(SearchType searchType, List<PersonPair> personPairList) {
-		PersonPair answer = personPairList.get(0);
-		for (PersonPair result : personPairList) {
-			switch (searchType) {
-				case Minimum:
-					if (result.dateDiff < answer.dateDiff) {
-						answer = result;
-					}
-					break;
-
-				case Maximum:
-					if (result.dateDiff > answer.dateDiff) {
-						answer = result;
-					}
-					break;
-			}
-		}
-		return answer;
-	}
-
-	private List<PersonPair> getPersonPairs() {
-		List<PersonPair> personPairList = new ArrayList<PersonPair>();
-
-		for (int i = 0; i < people.size() - 1; i++) {
-			for (int j = i + 1; j < people.size(); j++) {
-				PersonPair personPair = new PersonPair();
-				if (people.get(i).birthDate.getTime() < people.get(j).birthDate.getTime()) {
-					personPair.person1 = people.get(i);
-					personPair.person2 = people.get(j);
-				} else {
-					personPair.person1 = people.get(j);
-					personPair.person2 = people.get(i);
-				}
-				personPair.dateDiff = personPair.person2.birthDate.getTime() - personPair.person1.birthDate.getTime();
-				personPairList.add(personPair);
-			}
-		}
-		return personPairList;
+		return personFinder.Find(searchType);
 	}
 }
