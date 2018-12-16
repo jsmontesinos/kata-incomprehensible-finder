5679949010f54bccab52c6a69c8405c11da11fc7
Juan Sánchez <jsmontesinos@gmail.com>
2018-12-16 14:01:15 +0100

    Extract method

--- src/main/java/algorithm/Finder.java
+++ src/main/java/algorithm/Finder.java
@@ -10,22 +10,7 @@ public class Finder {
 	}
 
 	public PersonPair Find(SearchType searchType) {
-		List<PersonPair> personPairList = new ArrayList<PersonPair>();
-
-		for (int i = 0; i < people.size() - 1; i++) {
-			for (int j = i + 1; j < people.size(); j++) {
-				PersonPair personPair = new PersonPair();
-				if (people.get(i).birthDate.getTime() < people.get(j).birthDate.getTime()) {
-					personPair.person1 = people.get(i);
-					personPair.person2 = people.get(j);
-				} else {
-					personPair.person1 = people.get(j);
-					personPair.person2 = people.get(i);
-				}
-				personPair.dateDiff = personPair.person2.birthDate.getTime() - personPair.person1.birthDate.getTime();
-				personPairList.add(personPair);
-			}
-		}
+		List<PersonPair> personPairList = getPersonPairs();
 
 		if (personPairList.size() < 1) {
 			return new PersonPair();
@@ -49,5 +34,25 @@ public class Finder {
 		}
 
 		return answer;
+	}
+
+	private List<PersonPair> getPersonPairs() {
+		List<PersonPair> personPairList = new ArrayList<PersonPair>();
+
+		for (int i = 0; i < people.size() - 1; i++) {
+			for (int j = i + 1; j < people.size(); j++) {
+				PersonPair personPair = new PersonPair();
+				if (people.get(i).birthDate.getTime() < people.get(j).birthDate.getTime()) {
+					personPair.person1 = people.get(i);
+					personPair.person2 = people.get(j);
+				} else {
+					personPair.person1 = people.get(j);
+					personPair.person2 = people.get(i);
+				}
+				personPair.dateDiff = personPair.person2.birthDate.getTime() - personPair.person1.birthDate.getTime();
+				personPairList.add(personPair);
+			}
+		}
+		return personPairList;
 	}
 }
