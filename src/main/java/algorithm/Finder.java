464de4fe14e574486b043e37ed3bc1927fdcdfd1
Juan Sánchez <jsmontesinos@gmail.com>
2018-12-16 14:46:45 +0100

    Create two finder stratigies

--- src/main/java/algorithm/Finder.java
+++ src/main/java/algorithm/Finder.java
@@ -2,14 +2,26 @@ package algorithm;
 import java.util.List;
 
 public class Finder {
+	private List<Person> people;
 	private PersonFinder personFinder;
 
 	public Finder(List<Person> people) {
-		this.personFinder = new PersonFinder(people);
+		this.people = people;
 	}
 
 	public PersonPair Find(SearchType searchType) {
-
+		this.personFinder = getPersonFinderAccordingType(searchType);
 		return personFinder.Find(searchType);
+	}
+
+	private PersonFinder getPersonFinderAccordingType(SearchType searchType) {
+		switch (searchType) {
+			case Minimum:
+				return new PersonFinderByMinimumDateDiff(people);
+			case Maximum:
+				return new PersonFinderByMaximumDateDiff(people);
+		}
+
+		return null;
 	}
 }
